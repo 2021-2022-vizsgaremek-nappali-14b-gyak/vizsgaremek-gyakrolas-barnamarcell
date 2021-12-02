54204ed57a495ff0aeee9c8cbd269ed8007eb5c0
sz0barmar <sz0barmar@vasvari.org>
2021-12-02 09:41:53 +0100

    alakul

---
+++ Desktop/src/Vizsgaremek/Navigaton/Navigation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Vizsgaremek.Navigaton
+{
+     public class Navigation
+    {
+        private static MainWindow mainwindow;
+
+        public static void Navigate(UserControl userControl)
+        {
+            mainwindow.PageContent.Children.Add(userControl);
+        }
+    }
+}
