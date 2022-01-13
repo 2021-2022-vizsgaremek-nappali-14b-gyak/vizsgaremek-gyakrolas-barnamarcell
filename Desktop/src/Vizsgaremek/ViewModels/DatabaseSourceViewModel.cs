71a18c509e157cafd7baf7b027d4272549708885
sz0barmar <sz0barmar@vasvari.org>
2022-01-13 09:05:17 +0100

    Databasesource

---
+++ Desktop/src/Vizsgaremek/ViewModels/DatabaseSourceViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+using Vizsgaremek.Repositories;
+using Vizsgaremek.Models;
+
+namespace Vizsgaremek.ViewModels
+{
+    public class DatabaseSourceViewModel
+    {
+        private ObservableCollection<string> displayedDatabaseSources;
+        private string selectedDatabaseSource;
+        private DbSource dbSource;
+
+        DatabaseSouerces repoDatabaseSouerces;
+
+        public ObservableCollection<string> DisplayedDatabaseSources 
+        { 
+            get => displayedDatabaseSources; 
+        }
+        public string SelectedDatabaseSource 
+        { 
+            get => selectedDatabaseSource; 
+            set => selectedDatabaseSource = value; 
+        }
+
+        public DbSource DbSource
+        {
+            get
+            {
+                // TDD fejlesztés
+                return DbSource.NONE;
+            }
+        }
+
+        public DatabaseSourceViewModel()
+        {
+            repoDatabaseSouerces = new DatabaseSouerces();
+            displayedDatabaseSources = new ObservableCollection<string>(repoDatabaseSouerces.GetAllDatabaseSources());
+        }
+    }
+}
